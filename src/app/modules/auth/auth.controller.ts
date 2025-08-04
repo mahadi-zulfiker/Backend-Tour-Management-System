617dbeeb9aae0ac43b273b52b04c33f6f98eb059
mahadi-zulfiker <mahade.adib45@gmail.com>
2025-08-04 13:15:13 +0600

    cookie parser logic added for tokens

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,10 +4,21 @@ import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status-codes";
 import { AuthService } from "./auth.service";
+import AppError from "../../errprHelpers/AppError";
 
 const credentialsLogin = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const loginInfo = await AuthService.credentialsLogin(req.body);
+
+    res.cookie("accessToken", loginInfo.accessToken, {
+      httpOnly: true,
+      secure: false,
+    });
+
+    res.cookie("refreshToken", loginInfo.refreshToken, {
+      httpOnly: true,
+      secure: false,
+    });
 
     sendResponse(res, {
       statusCode: httpStatus.OK,
@@ -21,6 +32,11 @@ const credentialsLogin = catchAsync(
 const getNewAccessToken = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+      throw new AppError(httpStatus.UNAUTHORIZED, "Refresh token is missing");
+    }
+
     const tokenInfo = await AuthService.getNewAccessToken(refreshToken);
 
     sendResponse(res, {
